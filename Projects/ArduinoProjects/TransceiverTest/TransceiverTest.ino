f3ac936d3001b7079eae41499ff3ab268209b208
Asad Mohiuddin <39422242+Asmo97@users.noreply.github.com>
2018-09-04 19:24:30 +0100

    Burst mode added

--- Projects/ArduinoProjects/TransceiverTest/TransceiverTest.ino
+++ Projects/ArduinoProjects/TransceiverTest/TransceiverTest.ino
@@ -1,7 +1,7 @@
 #include <cstdint>
 
 #define ledPin 13 //builtin LED
-#define MOSFET_Driver_Pin 1
+#define MOSFET_Driver_PWM_Pin 21
 #define MOSFET_Pin 14
 
 enum State : std::uint8_t { OFFLINE = 1, TRANSMIT, LISTEN};
@@ -12,7 +12,7 @@ char rx_byte;
 void setup() {
   Serial.begin(9600);
   pinMode(ledPin, OUTPUT);
-  pinMode(MOSFET_Driver_Pin, OUTPUT);
+  pinMode(MOSFET_Driver_PWM_Pin, OUTPUT);
   pinMode(MOSFET_Pin, OUTPUT);
 }
 
@@ -41,10 +41,37 @@ void loop() {
   switch(Transducer){
     case OFFLINE: digitalWrite(ledPin, LOW); break;
 
-    case TRANSMIT: digitalWrite(ledPin, HIGH); digitalWrite(MOSFET_Driver_Pin, HIGH); digitalWrite(MOSFET_Pin, HIGH); break;
+    case TRANSMIT: digitalWrite(ledPin, HIGH); digitalWrite(MOSFET_Pin, HIGH); Generate_Burst(10, 5, 15); break;
 
-    case LISTEN: digitalWrite(ledPin, HIGH); digitalWrite(MOSFET_Driver_Pin, LOW); digitalWrite(MOSFET_Pin, LOW); break;
+    case LISTEN: digitalWrite(ledPin, HIGH); digitalWrite(MOSFET_Driver_PWM_Pin, LOW); digitalWrite(MOSFET_Pin, LOW); break;
     
   }
 }
 
+void Generate_Burst(int period, int cycles, int cycle_period){ //period in ms, cycle period in us
+  elapsedMillis timer;
+
+  while(timer <= period){
+    digitalWrite(MOSFET_Driver_PWM_Pin, HIGH);
+  }
+
+  //-------Now create the cycles--------
+
+  int current_cycles = 0;
+  elapsedMicros cycle_timer;
+  
+  while (current_cycles < cycles){
+    while (cycle_timer <= cycle_period) {
+      digitalWrite(MOSFET_Driver_PWM_Pin, LOW);
+    }
+    
+    while (cycle_timer > cycle_period && cycle_timer <= (cycle_period*2) ) {
+      digitalWrite(MOSFET_Driver_PWM_Pin, HIGH);
+    }
+  
+    current_cycles++;
+    cycle_timer = 0;
+  }
+  
+  timer = 0;
+}
